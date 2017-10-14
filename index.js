4ab7b95c13b716fe6f137bc5dace0863fbfaaf14
Nivesh <nivesh@admins-MacBook-Pro.local>
2017-10-14 20:13:30 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const assert = require('assert')
+
+/**
+ * Formats Joi's error.details array of messages to
+ * single message seperated by comma.
+ * @param {any} error 
+ * @returns string
+ */
+function parseJoiError(error) {
+  assert(error)
+  if (!error instanceof Error) {
+    throw new Error('error should be instanceOf Error Object')
+  }
+  let message = ''
+  if (error.details != null && Array.isArray(error.details)) {
+    message += error.details.reduce((p, v) => `${p + v.message},`, '')
+  } else {
+    message = error.message
+  }
+  return message
+}
+
+
+module.exports = parseJoiError
